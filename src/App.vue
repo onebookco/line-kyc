729d988a0db3f15d401353dcba7049608e113519
Kawin Glomjai <g.kawin@live.com>
2024-12-02 04:57:14 +0000

    init

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="container m-auto">
+    <h1 class="tex">กรุณายืนยันตัวตน</h1>
+    <img class="logo" src="./assets/logo.png" alt="Vue logo" />
+    <form>
+      <label for="username"></label>
+      <input type="text" id="username" name="username" class="input input-lg input-bordered" />
+    </form>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+
 </template>
 
 <style scoped>
@@ -21,9 +21,11 @@ import HelloWorld from './components/HelloWorld.vue'
   will-change: filter;
   transition: filter 300ms;
 }
+
 .logo:hover {
   filter: drop-shadow(0 0 2em #646cffaa);
 }
+
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
 }
